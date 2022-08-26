122443d4ae27196034cc2ac6a99d71a2e8cb79c9
theakhilleus <black_the_dragon@hotmail.com>
2022-08-26 14:01:52 +0300

    db delete action created

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-mongoose.connect("MongoDB Collection URL", { useNewUrlParser: true });
+mongoose.connect("MONGODB DB LİNK", { useNewUrlParser: true });
 
 const itemsSchema = {
   title: String,
@@ -40,7 +40,7 @@ const result = newItem.save();
 console.log(result);
 */
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   Item.find({}, function (err, foundItems) {
     if (foundItems.length === 0) {
       const message = "maalesef :/";
@@ -51,14 +51,16 @@ app.get("/", function (req, res) {
   });
 });
 
-app.post("/", function (req, res) {
+app.post("/", (req, res) => {
   const title = req.body.sendItem.title;
   const text = req.body.sendItem.text;
+  /* 
   console.log(req);
   console.log("-------------------------------------");
   console.log(req.body);
   console.log(req.body.sendItem);
   console.log("-------------------------------------");
+ */
 
   console.log(title, text);
   const item = new Item({
@@ -70,7 +72,19 @@ app.post("/", function (req, res) {
   const message = "Succesfully Handled The Request";
   res.send(message);
 });
-
+app.post("/delete", (req, res) => {
+  const _id = req.body.deleteItem._id;
+  console.log(_id);
+  // Delete the document by its _id
+  Item.deleteOne({ _id: _id })
+    .then(function () {
+      console.log("Data deleted"); // Success
+    })
+    .catch(function (error) {
+      console.log(error); // Failure
+    });
+  console.log("-------------------------------");
+});
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
